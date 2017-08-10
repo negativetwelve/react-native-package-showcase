e679a147ee3b0216f9068395f06a981ac5e3d280
Mark Miyashita <negativetwelve@gmail.com>
2017-08-10 14:49:57 -0700

    Move all examples to a separate directory

---
+++ js/examples/VideoExample.js
@@ -0,0 +1,17 @@
+// Libraries
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import Video from 'react-native-video';
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+
+// TODO(mark): We need to add a sample video to the repo.
+export default () => (
+  <View />
+);
