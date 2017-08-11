b986057c4c6ef435873acb7b08f5bcf473a088d8
Mark Miyashita <negativetwelve@gmail.com>
2017-08-10 18:47:05 -0700

    Create example component for react-native-heap-analytics

---
+++ js/examples/HeapAnalyticsExample.js
@@ -0,0 +1,14 @@
+// Libraries
+import React from 'react';
+import Heap from 'react-native-heap-analytics';
+
+
+export default class HeapAnalyticsExample extends React.Component {
+  componentDidMount() {
+    // Heap.setAppId('<YOUR APP ID>');
+  }
+
+  render() {
+    return null;
+  }
+}
