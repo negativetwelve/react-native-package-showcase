134e993acd2354b51aaf968a725bfd43b326e77b
Mark Miyashita <negativetwelve@gmail.com>
2017-08-10 18:51:34 -0700

    Add ShakeEventExample and render test

--- js/components/PackagesView.js
+++ js/components/PackagesView.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 
 // Packages
+import ShakeEventExample from '../examples/ShakeEventExample';
 import UXCamExample from '../examples/UXCamExample';
 import VideoExample from '../examples/VideoExample';
 
@@ -16,6 +17,7 @@ const styles = StyleSheet.create({
 
 export default () => (
   <View style={styles.container}>
+    <ShakeEventExample />
     <UXCamExample />
     <VideoExample />
   </View>
---
+++ js/examples/ShakeEventExample.js
@@ -0,0 +1,18 @@
+// Libraries
+import React from 'react';
+import ShakeEvent from 'react-native-shake-event';
+
+
+export default class ShakeEventExample extends React.Component {
+  componentDidMount() {
+    // TODO
+  }
+
+  componentWillUnmount() {
+    // TODO
+  }
+
+  render() {
+    return null;
+  }
+}
