0db3f1de76f8ccfcc4414ddf68fc6f0ef342c2d3
Mark Miyashita <negativetwelve@gmail.com>
2017-08-10 22:50:49 -0700

    Add IntercomExample for react-native-intercom

---
+++ js/examples/IntercomExample.js
@@ -0,0 +1,14 @@
+// Libraries
+import React from 'react';
+// import Intercom from 'react-native-intercom';
+
+
+export default class IntercomExample extends React.Component {
+  componentDidMount() {
+    // Intercom.registerIdentifiedUser({userId: '<user-id>'});
+  }
+
+  render() {
+    return null;
+  }
+}
